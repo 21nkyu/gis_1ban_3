f590824d2897d742d62ee383da7b5749a4439e6e
21nkyu <adele7178@naver.com>
2021-07-12 14:51:48 +0900

    21 07 12 001 ik_1ban_3 012

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,16 +6,18 @@ from accountapp.models import HelloWorld
 
 
 def hello_world(request):
-	if request.method == 'POST':
+    if request.method == 'POST':
 
-		temp = request.POST.get('hello_world_input')
+        temp = request.POST.get('hello_world_input')
 
-		new_hello_world = HelloWorld()
-		new_hello_world.text = temp
-		new_hello_world.save()
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
 
-		return render(request, 'accountapp/hello_world.html',
-					  context={'hello_world_output': new_hello_world})
-	else:
-		return render(request, 'accountapp/hello_world.html',
-					  context={'text': 'GET METHOD!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html',
+                      context={'hello_world_list': hello_world_list})
+    else:
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html',
+                      context={'hello_world_list': hello_world_list})
