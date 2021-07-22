816eeb418f95d5c0166478eb44aafa0286f6ba17
21nkyu <adele7178@naver.com>
2021-07-22 14:40:34 +0900

    21 07 21 002 ik_1ban_3 026

--- accountapp/views.py
+++ accountapp/views.py
@@ -14,24 +14,22 @@ from accountapp.models import HelloWorld
 
 @login_required
 def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == 'POST':
 
-            temp = request.POST.get('hello_world_input')
+    if request.method == 'POST':
 
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
+        temp = request.POST.get('hello_world_input')
 
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
 
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html',
-                          context={'hello_world_list': hello_world_list})
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html',
+                      context={'hello_world_list': hello_world_list})
 
 
 
