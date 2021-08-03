94d4de493ae094c01c63fb482c04679157feb8a8
21nkyu <adele7178@naver.com>
2021-08-03 21:23:51 +0900

    21 08 02 003~004 ik_1ban_3 041~42

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path
+from django.views.generic import TemplateView
+
+from articleapp.views import ArticleCreateView, ArticleDetailView
+
 urlpatterns = [
+    path('list/', TemplateView.as_view(), name='list'),
+    path('create/', ArticleCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail')
 
 ]
