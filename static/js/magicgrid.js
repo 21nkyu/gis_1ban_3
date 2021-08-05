1ea8c64ae25f8642bfe0b4b65d5799e098754fa9
21nkyu <adele7178@naver.com>
2021-08-06 00:56:42 +0900

    21 08 05 001 ik_1ban_3 047

--- static/js/magicgrid.js
+++ static/js/magicgrid.js
@@ -275,4 +275,20 @@ MagicGrid.prototype.listen = function listen () {
 };
 
 
+let magicGrid = new MagicGrid({
+  container: '.container',
+  animate: true,
+  gutter: 30,
+  static: true,
+  useMin: true
+
+})
+var masonrys = document.getElementsByTagName("img")
+
+for (let i =0; i < masonrys.length; i++) {
+  masonrys[i].addEventListener('load', function () {
+    magicGrid.positionItems();
+  }, false)
+}
+
 magicGrid.listen();
