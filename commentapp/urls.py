facb23f405a720246493d8bedb8acfa6b4948095
21nkyu <adele7178@naver.com>
2021-08-10 02:48:48 +0900

    21 08 09 001~006 ik_1ban_3

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path
+
+from commentapp.views import CommentCreateView, CommentDeleteView
 
 urlpatterns = [
+    path('create/', CommentCreateView.as_view(), name='create'),
+    path('delete/<int:pk>', CommentDeleteView.as_view(), name='delete')
 
 ]
