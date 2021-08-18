41d09caa8eb15161e332d9cc59a1a1e220cc2358
21nkyu <adele7178@naver.com>
2021-08-18 09:44:35 +0900

    21 08 18 001~002 ik_1ban_3

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView, ProjectDetailView
+from projectapp.views import ProjectCreateView, ProjectDetailView, ProjectListView
 
 app_name = 'projectapp' #이걸 해줘야 자동완성이 되고 reverse에 사용한다
 
 urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
-
+    path('list/', ProjectListView.as_view(), name='list'),
 ]
