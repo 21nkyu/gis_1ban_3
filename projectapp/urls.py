8bea703444e17f7caabc84a37a1980b3a06feb82
21nkyu <adele7178@naver.com>
2021-08-18 09:13:27 +0900

    21 08 12 006 ik_1ban_3 sub fix

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView
+from projectapp.views import ProjectCreateView, ProjectDetailView
 
 app_name = 'projectapp' #이걸 해줘야 자동완성이 되고 reverse에 사용한다
 
 urlpatterns = [
-    path('create/', ProjectCreateView.as_view(), name='create')
+    path('create/', ProjectCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
 
 ]
